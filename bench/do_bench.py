52e44f97eb25ac551e4f07468f69935fb7fd28ca
Illia Ostapyshyn <illia.ostapyshyn@stud.uni-hannover.de>
2022-04-12 18:22:28 +0200

    bench: Improve benchmarking script

--- bench/do_bench.py
+++ bench/do_bench.py
@@ -2,10 +2,16 @@
 
 import subprocess as sp
 import numpy as np
+import argparse
 import csv
+import os
 
-def run(path, n=10):
-    desc = '/'.join(path.split('/')[-2:])
+BENCHMARKS = 'benchmarks.txt'
+NTIMES = 1000
+DEST = '.'
+
+def run(path, n):
+    desc = os.sep.join(path.split(os.sep)[-2:])
     diff = np.zeros(n)
 
     for i in range(n):
@@ -18,29 +24,44 @@ def run(path, n=10):
     return diff
 
 if __name__ == '__main__':
-    with open('benchmarks.txt') as f:
+    parser = argparse.ArgumentParser(description='Run PAC-SW benchmarks.')
+    parser.add_argument('dest', metavar = 'OUTPUT', nargs = '?', default = DEST, type = str,
+                        help = f'destination directory for results (default: {DEST})')
+    parser.add_argument('-n', metavar = 'NTIMES', default = NTIMES, type = int,
+                        help = f'amount of samples to collect (default: {NTIMES})')
+    parser.add_argument('-i', metavar = 'INPUT', default = BENCHMARKS, type = str,
+                        help = f'source of benchmarks (default: {BENCHMARKS})')
+
+    args = parser.parse_args()
+
+    with open(args.i) as f:
         benchmarks = f.read().splitlines()
 
     pac = {}
     bare = {}
 
+    # Perform measurements
     for b in benchmarks:
         if b.startswith('#'):
             continue
-        name = b.split('/')[-1]
+        b = os.path.normpath(b)
+        name = b.split(os.sep)[-1]
 
-        bare[name] = run(b + '/bare')
-        pac[name] = run(b + '/pac')
+        bare[name] = run(os.sep.join([b, 'bare']), args.n)
+        pac[name] = run(os.sep.join([b, 'pac']), args.n)
 
     FIELDS = ['Benchmark', 'Mean', 'SD', 'Min', 'Max']
 
-    with open('results/bare.csv', 'w') as f:
+    # Write the results
+    os.makedirs(args.dest, exist_ok=True)
+
+    with open(os.path.join(args.dest, 'bare.csv'), 'w') as f:
         w = csv.writer(f)
         w.writerow(FIELDS)
         for k, v in bare.items():
             w.writerow([k, v.mean(), v.std(), v.min(), v.max()])
 
-    with open('results/pac.csv', 'w') as f:
+    with open(os.path.join(args.dest, 'pac.csv'), 'w') as f:
         w = csv.writer(f)
         w.writerow(FIELDS)
         for k, v in pac.items():
