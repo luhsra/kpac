51941ec38f288af80f77f6f4ee972677800beb61
Illia Ostapyshyn <illia.ostapyshyn@stud.uni-hannover.de>
2022-04-22 18:30:25 +0200

    bench: Revisit benchmark automatisation

---
+++ bench/bench.c
@@ -0,0 +1,128 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <sys/wait.h>
+
+static inline void timespec_diff(struct timespec *a, struct timespec *b,
+                                 struct timespec *result) {
+    result->tv_sec  = a->tv_sec  - b->tv_sec;
+    result->tv_nsec = a->tv_nsec - b->tv_nsec;
+    if (result->tv_nsec < 0) {
+        --result->tv_sec;
+        result->tv_nsec += 1000000000L;
+    }
+}
+
+static int measure(char *const argv[], double *result)
+{
+    pid_t cpid;
+    int wstatus;
+    struct timespec tp0, tp1, diff;
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tp0);
+    cpid = fork();
+
+    if (cpid == -1) {
+        PyErr_SetString(PyExc_OSError, strerror(errno));
+        return 1;
+    }
+
+    if (cpid == 0) {
+        execv(argv[0], argv);
+        perror("execve");
+        exit(127);
+    }
+
+    do {
+        waitpid(cpid, &wstatus, 0);
+    } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tp1);
+
+    if (WIFSIGNALED(wstatus)) {
+        PyErr_SetString(PyExc_RuntimeError, "program terminated due to signal.");
+        return 1;
+    } else if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus) != 0) {
+        PyErr_SetString(PyExc_RuntimeError, "terminated with non-zero return code.");
+        return 1;
+    }
+
+    timespec_diff(&tp1, &tp0, &diff);
+    *result = diff.tv_sec + diff.tv_nsec * 1e-9;
+
+    return 0;
+}
+
+static char **pylist_as_argv(PyObject *list)
+{
+    int n = PyList_Size(list);
+    char **argv = malloc((n+1) * sizeof(char *));
+    if (!argv) {
+        PyErr_SetString(PyExc_OSError, strerror(errno));
+        return NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        PyObject *item = PyList_GetItem(list, i);
+        if (PyUnicode_Check(item)) {
+            argv[i] = (char *) PyUnicode_AsUTF8(item);
+            continue;
+        }
+        if (PyBytes_Check(item)) {
+            argv[i] = PyBytes_AsString(item);
+            continue;
+        }
+
+        PyErr_SetString(PyExc_TypeError, "list items must be strings.");
+        return NULL;
+
+    }
+    argv[n] = NULL;
+
+    return argv;
+}
+
+static PyObject *bench_run(PyObject *self, PyObject *args)
+{
+    ((void) self);
+    PyObject *list;
+
+    if (!PyArg_ParseTuple(args, "O!", &PyList_Type, &list)) {
+        PyErr_SetString(PyExc_TypeError, "parameter must be a list.");
+        return NULL;
+    }
+
+    char **argv = pylist_as_argv(list);
+    if (!argv)
+        return NULL;
+
+    double result;
+    if (measure(argv, &result))
+        return NULL;
+
+    free(argv);
+
+    return PyFloat_FromDouble(result);
+}
+
+static PyMethodDef BenchMethods[] = {
+    {"run",  bench_run, METH_VARARGS, "Run program and measure time."},
+    {NULL, NULL, 0, NULL}, /* Sentinel */
+};
+
+static struct PyModuleDef benchmodule = {
+    PyModuleDef_HEAD_INIT,
+    "bench",   /* name of module */
+    NULL,      /* module documentation, may be NULL */
+    -1,        /* size of per-interpreter state of the module,
+                  or -1 if the module keeps state in global variables. */
+    BenchMethods,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
+
+PyMODINIT_FUNC PyInit_bench(void)
+{
+    return PyModule_Create(&benchmodule);
+}
--- bench/do_bench.py
+++ bench/do_bench.py
@@ -2,71 +2,147 @@
 
 import subprocess as sp
 import numpy as np
-import argparse
-import csv
-import os
+import argparse, os, sys
+import csv, json
+import bench
 
-BENCHMARKS = 'benchmarks.txt'
-NTIMES = 1000
 DEST = '.'
 
-def run(path, n):
-    desc = os.sep.join(path.split(os.sep)[-2:])
-    diff = np.zeros(n)
+PLUGIN_PATH = os.path.abspath('../gcc/pac_sw_plugin.so')
+PAC_MAPPER = os.path.abspath('../gcc/map_device.o')
+PROLOGUE = os.path.abspath('../gcc/prologue.s')
+EPILOGUE = os.path.abspath('../gcc/epilogue.s')
 
-    for i in range(n):
-        print(f'\r{desc}: {i+1}/{n}', end='')
-        out = sp.check_output(["./do_bench", path])
-        diff[i] = float(out)
+verbose = False
 
-    mean = diff.mean()
-    std = diff.std()
-    masked = np.ma.masked_outside(diff, mean-2*std, mean+2*std)
-    masked_count = np.ma.count_masked(masked)
-    print(": %.9f (%.1f %%); %d masked" % (mean, std/mean*100, masked_count))
+class Benchmark:
+    def __init__(self, j, b):
+        self.path = b['path']
+        self.name = b.get('name', os.path.basename(self.path))
+        self.warmup = b.get('warmup', j.get('warmup', 0))
+        self.ntimes = b.get('ntimes', j['ntimes'])
+        self.do_run = b.get('do_run', j['do_run'])
+        self.do_build = b.get('do_build', j['do_build'])
 
-    return masked
+    def do(self, cmd):
+        if not verbose:
+            cmd += ' > /dev/null'
+        argv = ['sh', '-xc' if verbose else '-c', cmd]
+        sp.check_call(argv, cwd=self.path)
+
+    def build(self):
+        self.do(self.do_build)
+
+    def meas(self):
+        diff = np.zeros(self.ntimes)
+
+        cmd = self.do_run
+        if not verbose:
+            cmd += ' > /dev/null'
+
+        cwd = os.getcwd()
+        os.chdir(self.path)
+
+        for i in range(-self.warmup, self.ntimes):
+            sys.stdout.write(f'\r{self.name}: {i+1}/{self.ntimes:<12d}\r')
+            sys.stdout.flush()
+            res = bench.run(['/bin/sh', '-c', cmd])
+            if i >= 0:
+                diff[i] = res
+
+        os.chdir(cwd)
+
+        mean = diff.mean()
+        std = diff.std()/mean*100
+        masked = np.ma.masked_outside(diff, mean-2*std, mean+2*std)
+        masked_count = np.ma.count_masked(masked)
+        sys.stdout.write(f'{self.name}: {mean:#.9f} ({std:#.3f}); {masked_count} outliers rejected\n')
+
+        return masked
+
+class Suite:
+    def __init__(self, path):
+        with open(path) as f:
+            j = json.load(f)
+
+        self.benchmarks = []
+        for b in j['benchmarks']:
+            self.benchmarks.append(Benchmark(j, b))
+
+    def build(self):
+        for b in self.benchmarks:
+            b.build()
+        # sp.check_call(["sudo", "sh", "-c", "sync; echo 3 > /proc/sys/vm/drop_caches"])
+
+    def meas(self):
+        results = {}
+        for b in self.benchmarks:
+            results[b.name] = b.meas()
+
+        return results
+
+def plugin_flags(plugin, args):
+    name = ''.join(os.path.basename(plugin).split('.')[:-1])
+
+    s = '-fplugin=' + plugin
+    for k, v in args.items():
+        s += ' -fplugin-arg-' + name + '-' + k + '=' + v
+
+    return s
+
+def dump(f, data):
+    FIELDS = ['name', 'mean', 'std', 'min', 'max']
+    with open(f, 'w') as f:
+        w = csv.writer(f)
+        w.writerow(FIELDS)
+        for k, v in data.items():
+            w.writerow([k, v.mean(), v.std(), v.min(), v.max()])
+
+def dump_raw(f, data):
+    with open(f, 'w') as f:
+        w = csv.writer(f)
+        v = data.values()
+        maxlen = max([len(i) for i in v])
+        w.writerow(data.keys())
+        for i in range(maxlen):
+            w.writerow([j[i] if i < len(j) else None for j in v])
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run PAC-SW benchmarks.')
-    parser.add_argument('dest', metavar = 'OUTPUT', nargs = '?', default = DEST, type = str,
-                        help = f'destination directory for results (default: {DEST})')
-    parser.add_argument('-n', metavar = 'NTIMES', default = NTIMES, type = int,
-                        help = f'amount of samples to collect (default: {NTIMES})')
-    parser.add_argument('-i', metavar = 'INPUT', default = BENCHMARKS, type = str,
-                        help = f'source of benchmarks (default: {BENCHMARKS})')
+    parser = argparse.ArgumentParser(description='Compiler and run PAC-SW benchmarks.')
+    parser.add_argument('suite', metavar='SUITE', nargs=1, type=str,
+                        help='input file with benchmark suite description')
+    parser.add_argument('dest', metavar='OUTPUT', nargs='?', default=DEST, type=str,
+                        help=f'destination directory for results (default: {DEST})')
+    parser.add_argument('-v', action='store_true', help='display commands when executing')
+    parser.add_argument('-r', action='store_true', help='retain raw measurement data')
 
     args = parser.parse_args()
+    verbose = args.v
 
-    with open(args.i) as f:
-        benchmarks = f.read().splitlines()
+    suite = Suite(args.suite[0])
 
-    pac = {}
-    bare = {}
+    os.environ['PAC_MAPPER'] = PAC_MAPPER
+    os.environ['PAC_FLAGS'] = plugin_flags(PLUGIN_PATH, { 'init-function': 'map_device' })
 
-    # Perform measurements
-    for b in benchmarks:
-        if b.startswith('#'):
-            continue
-        b = os.path.normpath(b)
-        name = b.split(os.sep)[-1]
-
-        bare[name] = run(os.sep.join([b, 'bare']), args.n)
-        pac[name] = run(os.sep.join([b, 'pac']), args.n)
-
-    FIELDS = ['name', 'mean', 'std', 'min', 'max']
+    print('Building without PA:')
+    suite.build()
+    data = suite.meas()
 
     # Write the results
     os.makedirs(args.dest, exist_ok=True)
 
-    with open(os.path.join(args.dest, 'bare.csv'), 'w') as f:
-        w = csv.writer(f)
-        w.writerow(FIELDS)
-        for k, v in bare.items():
-            w.writerow([k, v.mean(), v.std(), v.min(), v.max()])
+    dump(os.path.join(args.dest, 'nopac.csv'), data)
+    if args.r:
+        dump_raw(os.path.join(args.dest, 'nopac_raw.csv'), data)
 
-    with open(os.path.join(args.dest, 'pac.csv'), 'w') as f:
-        w = csv.writer(f)
-        w.writerow(FIELDS)
-        for k, v in pac.items():
-            w.writerow([k, v.mean(), v.std(), v.min(), v.max()])
+    os.environ['PAC_FLAGS'] = plugin_flags(PLUGIN_PATH, { 'init-function': 'map_device',
+                                                          'prologue': PROLOGUE,
+                                                          'epilogue': EPILOGUE })
+
+    print('Building with PA:')
+    suite.build()
+    data = suite.meas()
+
+    dump(os.path.join(args.dest, 'pac.csv'), data)
+    if args.r:
+        dump_raw(os.path.join(args.dest, 'pac_raw.csv'), data)
