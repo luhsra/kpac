4010a5db843e1b09797c8d75d6e91a28269b794a
Illia Ostapyshyn <illia.ostapyshyn@stud.uni-hannover.de>
2022-09-19 00:26:57 +0200

    bench: Add thesis notebooks

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((eval . (org-babel-do-load-languages 'org-babel-load-languages
+                                                   '((python . t)))))))
--- bench/notebook/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((org-mode . ((eval . (org-babel-do-load-languages 'org-babel-load-languages
-                                                   '((python . t)))))))
